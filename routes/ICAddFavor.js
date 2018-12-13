4068e1b962004ceb3a050ee3fab94090958fd543
mayo <chienduan18@gmail.com>
2018-12-13 15:23:33 +0800

    添加移除專案api

--- routes/ICAddFavor.js
+++ routes/ICAddFavor.js
@@ -21,17 +21,20 @@ router
       function (error, results) {
         if (error) throw error;
         if (results.length == 1) {
-          res.json({ message: '已收藏' });
+          res.json({
+            message: '已收藏'
+          });
           console.log("a")
-        }
-        else {
+        } else {
           //之前沒收藏 => 可以收藏
           connection.query(
             "INSERT INTO `ic_favor`(`BScase_sid`, `ICmember_sid`) VALUES (?,?)",
             [req.body.BScase_sid, req.body.ICmember_sid],
             function (error) {
               if (error) throw error;
-              res.json({ message: "收藏成功" });
+              res.json({
+                message: "收藏成功"
+              });
               console.log("b")
             }
           );
@@ -50,83 +53,47 @@ router.route("/ICAddFavor/:sid")
   .get(function (req, res) {
     //顯示廠商po的案子列表
     connection.query(
-      "SELECT * FROM `ic_favor`o JOIN `bs_case` p ON o.BScase_sid =p.BScase_sid  WHERE o.ICmember_sid=? AND p.BS_state=1",  //IC_sid為廠商的id
+      "SELECT * FROM `ic_favor`o JOIN `bs_case` p ON o.BScase_sid =p.BScase_sid  WHERE o.ICmember_sid=? AND p.BS_state=1", //IC_sid為廠商的id
       req.params.sid,
       function (error, results) {
         if (error) throw error;
-        res.json(results);  //回傳 [{},{}....]
+        res.json(results); //回傳 [{},{}....]
       }
     );
   })
 
+
+
+
+
+//   http://localhost:3000/api/ICGetFavor/1/2
+router
+  .route("/ICGetFavor/:ICsid/:BScsaseid")
+  .get(function (req, res) { //用ICmember_sid和BScase_sid取得sid 
+    connection.query("SELECT `sid` FROM `ic_favor` WHERE `ICmember_sid`=? AND `BScase_sid`=?", [req.params.ICsid, req.params.BScsaseid], function (error, results) {
+      if (error) throw error;
+      res.json(results)
+    })
+  })
+  .delete(function (req, res) { //用ICmember_sid和BScase_sid 刪除該筆收藏 
+    connection.query("DELETE FROM  ic_favor WHERE ICmember_sid=? AND BScase_sid=? ", [req.params.ICsid, req.params.BScsaseid], function (error) {
+      if (error) throw error;
+      res.json({
+        message: "移除成功！"
+      })
+    })
+  });
+
+
+//   http://localhost:3000/api/ICFavorBSC/1
+router
+  .route("/ICFavorBSC/:ICsid")
+  .get(function (req, res) { //用ICmember_sid 拿到所有收藏的BS BScase 
+    connection.query("SELECT BS.* FROM `icmember` BS JOIN bs_favor icf ON icf.ICmember_sid=? AND BS.BScase_sid = icf.BScase_sid ORDER BY BS.BScase_sid", [req.params.ICsid], function (error, results) {
+      if (error) throw error;
+      res.json(results)
+    })
+  });
+
+
 module.exports = router;
-
-
-// var express = require('express');
-// var router = express.Router();
-// var mysql = require("mysql");
-
-
-// //建立連線
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   database: "u04",
-//   user: "root",
-//   password: "",
-// });
-
-// // connection.connect();
-// connection.connect(function (err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
-
-// //localhost/3000/api/ICAddFavor
-// router.route("/ICAddFavor")
-//   .post(function (req, res) {//收藏專案
-//     connection.query("insert into ic_favor SET ?", req.body, function (error) {
-//       if (error) throw error;
-//       res.json({ message: "已收藏" });
-//     })
-//   })
-//   .get(function (req, res) {//收藏專案
-//     connection.query("select * from ic_favor", function (error, rows) {
-//       if (error) throw error;
-//       res.json(rows)
-//       // res.json({message:"get成功"});
-//     })
-//   });
-
-
-
-// router
-//   .route("/ICAddFavor/:id")
-//   .get(function (req, res) {
-//     connection.query("SELECT `bscase_sid` FROM `ic_favor` WHERE `ic_sid`=?", req.params.id, function (error, rows) {
-//       if (error) throw error;
-//       res.json(rows);
-//     })
-//   })
-
-//   .delete(function (req, res) {//刪除收藏
-//     connection.query("DELETE FROM  ic_favor WHERE if_sid=?", req.params.id, function (error, results) {
-//       if (error) throw error;
-//       res.json({ message: "刪除成功！" })
-//     })
-//   });
-
-// //   http://localhost:3000/api/icGetFavor/1/2
-// router
-//   .route("/ICGetFavor/:icsid/:bscasesid'")
-//   .get(function (req, res) {//取得ic_sid收藏
-//     connection.query("SELECT `if_sid` FROM `ic_favor` WHERE `ic_sid`=? AND `bscase_sid`=?", [req.params.bscasesid, req.params.bscase_sid], function (error, results) {
-//       if (error) throw error;
-//       res.json(results)
-//     })
-//   });
-
-
-// module.exports = router;
